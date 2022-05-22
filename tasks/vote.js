cb77f76025b377631bc4eacd308244166c6abfbc
ded <46560089+987qwerty@users.noreply.github.com>
2022-05-22 23:09:07 +0300

    Update vote.js

--- tasks/vote.js
+++ tasks/vote.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 
-const abi = ["function vote(uint votingNumber, address proposal) payable"]
+const abi = ["function vote(uint votingNumber, uint proposalId) payable"]
 
 task("vote", "Vote for a proposal")
-    .addParam("num", "Number of voting") // example of input: npx hardhat vote --num 0 --adr 0xb6732A7F4183D7f951374a43C8319603889Be7ca
+    .addParam("num", "Number of voting") // example of input: npx hardhat vote --num 0 --adrId 0
     .addParam("adr", "Addresses of proposal to vote")
     .setAction(async (taskArgs, hre) => {
     const contract = await new ethers.Contract(`${process.env.address}`, abi, (await hre.ethers.getSigners())[0]);
-    tx = await contract.vote(taskArgs.num, taskArgs.adr, {value: 10000000000000000n});
+    tx = await contract.vote(taskArgs.num, taskArgs.adrId, {value: 10000000000000000n});
     console.log("tx hash: ", tx.hash)
 });
